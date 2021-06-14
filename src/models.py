de8d3e24de7f141590a9f989d001d6b672a7a706
SurajDonthi <surajdonthi95@gmail.com>
2021-06-14 05:45:02 +0000

    Added ConvolutionModule

--- src/models.py
+++ src/models.py
@@ -97,18 +97,64 @@ class ResidulLayerNorm(nn.Module):
 
 class FeedForward(nn.Module):
 
-    def __init__(self, input_dim: int = 512, expansion_factor: int = 4, dropout_prob: int = 0.1):
+    def __init__(self, input_dim: int = 512,
+                 expansion_factor: int = 4,
+                 drop_prob: int = 0.1):
         super().__init__()
-        self._layers(input_dim, expansion_factor, dropout_prob)
+        self._layers(input_dim, expansion_factor, drop_prob)
 
-    def _layers(self, input_dim, expansion_factor, dropout_prob):
+    def _layers(self, input_dim, expansion_factor, drop_prob):
         self.linear1 = nn.Linear(input_dim, expansion_factor)
-        self.dropout1 = nn.Dropout(dropout_prob)
+        self.dropout1 = nn.Dropout(drop_prob)
         self.linear2 = nn.Linear(expansion_factor, input_dim)
-        self.dropout2 = nn.Dropout(dropout_prob)
+        self.dropout2 = nn.Dropout(drop_prob)
 
     def forward(self, x):
         # Swish Activation Function
         x = self.dropout1(F.silu(self.linear1(x)))
         x = self.dropout2(self.linear2(x))
         return x
+
+
+class ConvolutionModule(nn.Module):
+
+    def __init__(self, input_dim: int,
+                 expansion_factor: int = 4,
+                 depth_kernel_size: int = 31,
+                 drop_prob: int = 0.1):
+        super().__init__()
+        self._layers(input_dim, expansion_factor, depth_kernel_size, drop_prob)
+
+    def _layers(self, input_dim: int,
+                expansion_factor: int = 4,
+                depth_kernel_size: int = 31,
+                drop_prob: int = 0.1):
+        self.layer_norm = nn.LayerNorm(input_dim)
+        self.pointwise_conv1 = nn.Conv2d(input_dim,
+                                         input_dim * expansion_factor,
+                                         kernel_size=1,
+                                         stride=1,
+                                         padding=0
+                                         )
+        self.depthwise_conv = nn.Conv1d(input_dim,
+                                        input_dim * expansion_factor,
+                                        kernel_size=depth_kernel_size,
+                                        stride=1,
+                                        padding=(depth_kernel_size - 1) // 2,
+                                        bias=False
+                                        )
+        self.pointwise_conv2 = nn.Conv2d(input_dim,
+                                         input_dim * expansion_factor,
+                                         kernel_size=1,
+                                         stride=1,
+                                         padding=0
+                                         )
+        self.dropout = nn.Dropout(drop_prob)
+
+    def forward(self, x):
+        x = self.layer_norm(x).transpose(1, 2).contiguous()
+        x = F.glu(self.pointwise_conv1(x))
+        x = F.silu(F.batch_norm(self.depthwise_conv(x)))
+
+        x = self.dropout(self.pointwise_conv2(x))
+        return x.transpose(1, 2).contiguous()
