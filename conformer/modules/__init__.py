259007a17fd93ed0baf7ab7add95350413559eb2
SurajDonthi <surajdonthi95@gmail.com>
2021-06-14 07:33:08 +0000

    Updated env. Added test for MultiHeadAttention

--- conformer/modules/__init__.py
+++ conformer/modules/__init__.py
@@ -1,1 +1,4 @@
-from .models import *
+from .conformer_block import ConformerBlock
+from .convoluion_module import ConvolutionModule
+from .feedforward import FeedForward
+from .multihead_self_attention import MultiHeadSelfAttention
--- tests/modules/test_multihead_self_attention.py
+++ tests/modules/test_multihead_self_attention.py
@@ -1,1 +1,10 @@
 import pytest
+import torch as th
+from conformer.modules import MultiHeadSelfAttention
+
+
+def test_multihead_self_attention():
+    mha = MultiHeadSelfAttention(embed_size=512, heads=8, mask=True)
+    tensor = th.tensor(32, 128, 512)
+    atten_out = mha(tensor)
+    assert atten_out.shape = tensor.shape
