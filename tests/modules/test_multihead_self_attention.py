89f7cbb52d0d6c8f4ef8b60af07730c0029fbf51
SurajDonthi <surajdonthi95@gmail.com>
2021-06-14 08:01:31 +0000

    Updated env and fixed bug in MHA test

--- tests/modules/test_multihead_self_attention.py
+++ tests/modules/test_multihead_self_attention.py
@@ -5,6 +5,6 @@ from conformer.modules import MultiHeadSelfAttention
 
 def test_multihead_self_attention():
     mha = MultiHeadSelfAttention(embed_size=512, heads=8, mask=True)
-    tensor = th.tensor(32, 128, 512)
+    tensor = th.rand(32, 128, 512)
     atten_out = mha(tensor)
-    assert atten_out.shape = tensor.shape
+    assert atten_out.shape == tensor.shape
