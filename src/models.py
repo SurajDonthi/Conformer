14c25a5c9d11a27c6be4a5fa176d67726d8530e0
SurajDonthi <surajdonthi95@gmail.com>
2021-06-14 04:38:48 +0000

    Added FeedForward model

--- src/models.py
+++ src/models.py
@@ -93,3 +93,22 @@ class ResidulLayerNorm(nn.Module):
 
     def forward(self, *tensors: th.Tensor):
         return self.layer_norm(self.dropout(*tensors) + tensors[-1])
+
+
+class FeedForward(nn.Module):
+
+    def __init__(self, input_dim: int = 512, expansion_factor: int = 4, dropout_prob: int = 0.1):
+        super().__init__()
+        self._layers(input_dim, expansion_factor, dropout_prob)
+
+    def _layers(self, input_dim, expansion_factor, dropout_prob):
+        self.linear1 = nn.Linear(input_dim, expansion_factor)
+        self.dropout1 = nn.Dropout(dropout_prob)
+        self.linear2 = nn.Linear(expansion_factor, input_dim)
+        self.dropout2 = nn.Dropout(dropout_prob)
+
+    def forward(self, x):
+        # Swish Activation Function
+        x = self.dropout1(F.silu(self.linear1(x)))
+        x = self.dropout2(self.linear2(x))
+        return x
