c96edc7a50b3888035d91cea6d7f16230aeb0356
SurajDonthi <surajdonthi95@gmail.com>
2021-06-14 08:15:19 +0000

    Added new MHA test

--- tests/modules/test_multihead_self_attention.py
+++ tests/modules/test_multihead_self_attention.py
@@ -1,9 +1,15 @@
-import pytest
 import torch as th
 from conformer.modules import MultiHeadSelfAttention
 
 
-def test_multihead_self_attention():
+def test_input_with_default_values():
+    mha = MultiHeadSelfAttention()
+    tensor = th.rand(32, 128, 512)
+    atten_out = mha(tensor)
+    assert atten_out.shape == tensor.shape
+
+
+def test_input_with_mask_true():
     mha = MultiHeadSelfAttention(embed_size=512, heads=8, mask=True)
     tensor = th.rand(32, 128, 512)
     atten_out = mha(tensor)
