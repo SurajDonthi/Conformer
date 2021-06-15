bc8e2b63f3470b956f3c68092e0df708e04e0a33
SurajDonthi <surajdonthi95@gmail.com>
2021-06-15 13:44:10 +0000

    Added input arguments

--- conformer/modules/conformer_encoder.py
+++ conformer/modules/conformer_encoder.py
@@ -7,15 +7,15 @@ from .convolution_module import Conv2dSubsampling
 
 class ConformerEncoder(nn.Module):
 
-	def __init__(self, ):
-		super().__init__()
-		self._layers()
+    def __init__(self, input_dim: int = 80, encoder_dim: int = 512, num_layers: int = 5):
+        super().__init__()
+        self._layers(input_dim, encoder_dim, num_layers)
 
-	def _layers(self, ):
-		self.conv2d_subsampling = Conv2dSubsampling(1, encoder_dim)
-		self.layers = nn.ModuleList(
-			[ConformerBlock()] * num_layers)
+    def _layers(self, input_dim: int = 80, encoder_dim: int = 512, num_layers: int = 5):
+        self.conv2d_subsampling = Conv2dSubsampling(1, encoder_dim)
+        self.layers = nn.ModuleList(
+            [ConformerBlock()] * num_layers)
 
-	def forward(self, x):
-		
-		return x
+    def forward(self, x):
+
+        return x
