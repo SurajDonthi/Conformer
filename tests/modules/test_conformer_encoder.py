950a1a653642c905237869aabceb9c501b77e04a
SurajDonthi <surajdonthi95@gmail.com>
2021-06-16 11:41:02 +0000

    Fixed dimension bug

--- tests/modules/test_conformer_encoder.py
+++ tests/modules/test_conformer_encoder.py
@@ -1,15 +1,18 @@
 import pytest
 import torch as th
 from conformer.modules import ConformerEncoder
+from loguru import logger
 
 
 def test_input_with_default_values():
     batch_size, sequence_length, dim = 3, 12345, 80
     inputs = th.rand(batch_size, sequence_length, dim)
     input_lengths = th.IntTensor([12345, 12300, 12000])
-    model = ConformerEncoder()
-    outputs, output_lenth = model(inputs, input_lengths)
-    assert outputs.shape == inputs.shape
+    model = ConformerEncoder(num_layers=1)
+    outputs, output_length = model(inputs, input_lengths)
+    logger.info(
+        f'Outputs shape: {outputs.shape}, Outputs length: {output_length}')
+    assert outputs.shape == th.Size([3, 3085, 512])
 
 
 @pytest.mark.skipif(not th.cuda.is_available(), reason='Skipping Test! GPU not Available!')
