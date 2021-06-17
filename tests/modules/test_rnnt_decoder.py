dd4c322ebde6408bc6d2d56e629ba7079edfc1d1
SurajDonthi <surajdonthi95@gmail.com>
2021-06-17 16:27:14 +0000

    Formatted code.

--- tests/modules/test_rnnt_decoder.py
+++ tests/modules/test_rnnt_decoder.py
@@ -10,7 +10,7 @@ def test_input_with_default_values():
                              [1, 3, 3, 3, 3, 3, 4, 5, 2, 0],
                              [1, 3, 3, 3, 3, 3, 4, 2, 0, 0]])
     target_lengths = th.LongTensor([9, 8, 7])
-    model = RNNTDecoder(num_classes, hidden_state_dim, num_layers, output_dim,
+    model = RNNTDecoder(num_classes, hidden_state_dim, output_dim, num_layers,
                         dropout_p=drop_p)
     outputs, output_length = model(targets, target_lengths)
     logger.info(
